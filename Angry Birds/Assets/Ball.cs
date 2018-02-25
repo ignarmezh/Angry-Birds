d61b5b1ce84c58f9cb453d00f4468129f5e5fe3d
iGNiTe <ignarmezh@gmail.com>
2018-02-25 18:42:24 +0300

    Create basic gameplay

---
+++ Angry Birds/Assets/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+
+    public float releaseTime = .15f;
+    public float maxDragDistance = 3f;
+
+    private bool isPressed = false;
+
+    private void Start()
+    {
+        rb.isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos,hook.position) < maxDragDistance)
+                rb.position = mousePos;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        this.enabled = false;
+    }
+}
---
+++ Angry Birds/Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float health = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
